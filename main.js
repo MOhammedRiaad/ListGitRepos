9218bb0fa9ab257c91460cc43506d73e8044a30b
Mohammed Riaad <MOhammed.riaad@gmail.com>
2020-06-04 15:06:38 +0200

    initil commit

---
+++ main.js
@@ -0,0 +1,45 @@
+
+// global def 
+
+let list = document.getElementById("portfolio");
+
+async function getUserAsync() 
+{
+  let response = await fetch(`https://api.github.com/users/MOhammedRiaad/repos`);
+  let data = await response.json()
+  return data;
+}
+
+getUserAsync()
+  .then(data => {
+    list.innerHTML='';
+    list.insertAdjacentHTML('beforeend',`<div class="row featured-entries-col featured-entries-logo">`)
+    let myrepo = data.filter(x=> x.fork != true)
+
+    Array.from(myrepo).forEach(element => {
+
+      
+      list.firstChild.insertAdjacentHTML('beforeend',`
+      
+      <div class="single-list col-sm-4 col-md-4 column">
+      <div class="single-item init-animate zoomIn" style="visibility: visible; animation-name: zoomIn;">
+          <div class="icon clearfix">
+              <a href="http://www.mohamedriaad.me/web-design/" class="all-link"> <i class="fa fa-paint-brush"></i>
+              </a> </div>
+          <h3 class="title">
+              <a href="http://www.mohamedriaad.me/web-design/" class="all-link">${element.name}</a> </h3>
+          <div class="content">
+              <div class="details">
+                  <p>${(element.description != null ?  element.description   : "No description yet")}</p>
+              </div>
+          </div>
+      </div>
+  </div>
+      `)
+    })
+    list.insertAdjacentHTML('afterend',`</div>`)
+
+  }
+
+
+    );
